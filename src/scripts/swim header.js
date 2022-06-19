08a1371eea81fb4e65750a9042838827be4bd84e
Данила Гаврютенков <pogranichnik.marshal@yandex.ru>
2022-06-19 21:55:29 +0300

    Модификация шапки.

--- src/scripts/swim header.js
+++ src/scripts/swim header.js
@@ -3,9 +3,10 @@ const header = document.querySelector(".nav_header");
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 4) {
         header.style.boxShadow = "0 4px 15px 0";
-        console.log(window.pageYOffset);
-
+        header.style.backgroundColor = "#fff";
+        
     } else if (window.pageYOffset <= 4) {
         header.style.boxShadow = "";
+        header.style.backgroundColor = "";
     }
 })
