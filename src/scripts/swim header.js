ce9d07dece1cc478b634484dbe0bd55dea8ff86e
Данила Гаврютенков <pogranichnik.marshal@yandex.ru>
2022-06-19 19:20:11 +0300

    Доработка плавающей шапки.

--- src/scripts/swim header.js
+++ src/scripts/swim header.js
@@ -3,6 +3,7 @@ const header = document.querySelector(".nav_header");
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 4) {
         header.style.boxShadow = "0 4px 15px 0";
+        header.style.backgroundColor = "#fff";
 
     } else if (window.pageYOffset <= 4) {
         header.style.boxShadow = "";
