d921997347b6e83c4ec9a7030b29c07782a75bd5
Данила Гаврютенков <pogranichnik.marshal@yandex.ru>
2022-06-19 12:02:14 +0300

    Удаление ненужных файлов и лишних строк кода.

--- docs/scripts/swim header.js
+++ docs/scripts/swim header.js
@@ -3,7 +3,6 @@ const header = document.querySelector(".nav_header");
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 4) {
         header.style.boxShadow = "0 4px 15px 0";
-        console.log(window.pageYOffset);
 
     } else if (window.pageYOffset <= 4) {
         header.style.boxShadow = "";
