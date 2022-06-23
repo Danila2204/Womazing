7fa8974ddfc63c11c83c72e2f375ea6404e5cf51
Данила Гаврютенков <pogranichnik.marshal@yandex.ru>
2022-06-23 18:16:05 +0300

    Создание работающего слайдера.

--- dist/scripts/slider.js
+++ dist/scripts/slider.js
@@ -1,6 +1,8 @@
 const slides = document.querySelectorAll(".gallery__img");
 const conditionSlides = document.querySelectorAll(".condition__img");
 
+let slidesPath = ["./imports/img/image-3.png", "./imports/img/image-2.png", "./imports/img/image-4.png"]
+
 const conditionSlidesInit = () => {
     let activePath = "./imports/img/condition.svg";
     let disActivePath = "./imports/img/condition_false.svg";
@@ -20,36 +22,68 @@ const slidesPositionInit = () => {
         document.querySelector(`.gallery__img_${i}`).style.position = "absolute";
 
         if (i === 1) {
-            document.querySelector(`.gallery__img_${i}`).style.left = `${parseFloat(bodyWidth) / (1600 / 945)}px`;
-        } else if (i === 2) {
             document.querySelector(`.gallery__img_${i}`).style.left = `${parseFloat(bodyWidth) / (1600 / 815)}px`;
+        } else if (i === 2) {
+            document.querySelector(`.gallery__img_${i}`).style.left = `${parseFloat(bodyWidth) / (1600 / 945)}px`;
         } else if (i === 3) {
             document.querySelector(`.gallery__img_${i}`).style.left = `${parseFloat(bodyWidth) / (1600 / 1263)}px`;
         }
     }
 }
 
-const slider = {
-    condition: 2,
-    conditionInit() {
-        for (let i = 0; i < slides.length; i++) {
-            if (slides[i].className === "gallery__img_1") {
-                console.log("Первый слайд!");
-            } else {
-                console.log("Пофиг какой слайд.");
-            }
-        }
-        
-        console.log(i);
-    },
-    left() {
-        this.condition--;
-    },
-    right() {
-        this.condition++;
+const slideInit = () => {
+    if (slider.condition === 1) {
+        slidesPath = ["./imports/img/image-2.png", "./imports/img/image-4.png", "./imports/img/image-3.png"]
+    } else if (slider.condition === 2) {
+        slidesPath = ["./imports/img/image-3.png", "./imports/img/image-2.png", "./imports/img/image-4.png"]
+    } else if (slider.condition === 3) {
+        slidesPath = ["./imports/img/image-4.png", "./imports/img/image-3.png", "./imports/img/image-2.png"]
+    }
+
+    for (let i = 0; i < slidesPath.length; i++) {        
+        slides[i].setAttribute("src", slidesPath[i]);
     }
 }
 
-conditionSlidesInit();
+const slider = {
+    condition: 2,
+    buttonDirectory: false,
+    left() {
+        if (this.condition === 1) {
+            this.condition = 3;
+        } else {
+            this.condition--;
+        }
+
+        this.buttonDirectory = true;
+        slideInit();
+        conditionSlidesInit();
+    },
+    right() {
+        if (this.condition === 3) {
+            this.condition = 1;
+        } else {
+            this.condition++;
+        }
+
+        this.buttonDirectory = false;
+        slideInit();
+        conditionSlidesInit();
+    }
+}
+
+for (let i = 0; i < slides.length; i++) {
+    slides[i].addEventListener("mousemove", () => {
+        slides[i].style.cursor = "pointer";
+    })
+
+    slides[i].addEventListener("click", () => {
+        if (slides[i].classList[1] === "gallery__img_1") {
+            slider.left();
+        } else if (slides[i].classList[1] === "gallery__img_3") {
+            slider.right();
+        }
+    })
+}
+
 slidesPositionInit();
-slider.conditionInit();
